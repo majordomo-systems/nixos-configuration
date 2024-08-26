d1f05352981b27165b5258ce6260122e5711217d
majordomo-admin <admin@majordomo.systems>
2024-08-26 16:37:49 -0400

    Latest Commit;

--- applications/tmux.nix
+++ applications/tmux.nix
@@ -12,26 +12,60 @@
     secureSocket = false;
 
     plugins = with pkgs; [
+      tmuxPlugins.sensible
+      tmuxPlugins.resurrect
+      tmuxPlugins.continuum
+      tmuxPlugins.yank
       tmuxPlugins.better-mouse-mode
       tmuxPlugins.catppuccin
       # tmuxPlugins.dracula
     ];
 
     extraConfig = ''
-      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-      set -g prefix ^b    # Sets the prefix to Ctrl + b
       
-      set -g default-terminal "xterm-256color"
+      set -g prefix ^b                                        # Sets the prefix to Ctrl + b 
+      set -g default-terminal "tmux-256color"
       set -ga terminal-overrides ",*256col*:Tc"
       set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
       set-environment -g COLORTERM "truecolor"
+      set-option -g mouse on                                  # Mouse works as expected
+      set -g set-clipboard on                                 # Use system clipboard
+      set -g @fzf-url-fzf-options '-p 60%,30% --prompt="   " --border-label=" Open URL "'
+      set -g @fzf-url-history-limit '2000'
+      # set -g status-position top                            # MacOS / darwin style
 
-      # Mouse works as expected
-      set-option -g mouse on
       # easy-to-remember split pane commands
       bind | split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
       bind c new-window -c "#{pane_current_path}"
+
+      # Start windows and panes at 1 not 0
+      set -g base-index 1
+      set -g pane-base-index 1
+      set-window-option -g pane-base-index 1
+      set-option -g renumber-windows on
+
+      # Open panes in current directory
+      bind '"'split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+
+      # set -g @catppuccin_window_left_separator ""
+      # set -g @catppuccin_window_right_separator " "
+      # set -g @catppuccin_window_middle_separator " █"
+      # set -g @catppuccin_window_number_position "right"
+      # set -g @catppuccin_window_default_fill "number"
+      # set -g @catppuccin_window_default_text "#W"
+      # set -g @catppuccin_window_current_fill "number"
+      # set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+      # set -g @catppuccin_status_modules_right "directory meetings date_time"
+      # set -g @catppuccin_status_modules_left "session"
+      # set -g @catppuccin_status_left_separator  " "
+      # set -g @catppuccin_status_right_separator " "
+      # set -g @catppuccin_status_right_separator_inverse "no"
+      # set -g @catppuccin_status_fill "icon"
+      # set -g @catppuccin_status_connect_separator "no"
+      # set -g @catppuccin_directory_text "#{b:pane_current_path}"
+      # set -g @catppuccin_date_time_text "%H:%M"
     '';
   };
 
