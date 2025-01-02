81ce1d86ef53e0146722760e63bcf550faa67552
majordomo-admin <admin@majordomo.systems>
2025-01-02 15:09:32 -0500

    Latest Commit;

---
+++ distros/macos/macos-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# wget https://raw.githubusercontent.com/majordomo-systems/nixos-configuration/main/distros/macos/macos-install.sh
+
+# NIX INSTALLATION
+
+
+# NIX CONFIGURATION
+cd ~/.config/nix/
+wget https://raw.githubusercontent.com/majordomo-systems/nixos-configuration/main/distros/macos/flake.nix
