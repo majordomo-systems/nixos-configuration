d9a7e34b521ab24fb87f180d81e3e8553c1830f7
majordomo-admin <admin@majordomo.systems>
2025-01-20 09:12:35 -0500

    Latest Commit;

--- configuration.nix
+++ configuration.nix
@@ -135,6 +135,11 @@
     desktopManager.gnome.enable = true;
   };
   # ##################################################################################### #
+  # Enable Tailscale
+  services.tailscale.enable = true;
+  # networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
+  # networking.search = [ "example.ts.net" ];
+  # ##################################################################################### #
   # Enable Docker
   virtualisation.docker.enable = true;
   # ##################################################################################### #
